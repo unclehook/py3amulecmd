3414783c8ad072c65942870a06fd677e578cc4c1
unclehook <uncle.hook@gmail.com>
2016-11-02 15:52:18 +0100

    now accept parameters

--- pyamulecmd.py
+++ pyamulecmd.py
@@ -2,23 +2,70 @@ import cmd
 import ec
 import getpass
 import sys
+from getopt import getopt, GetoptError
 
-prog_name = "pyamulecmd"
+prog_name = "py3amulecmd"
 prog_ver  = "0.1"
 
-host = b"192.168.149.109"
-port = 4712
-
 class REPL(cmd.Cmd):
+    
     def __init__(self):
-        cmd.Cmd.__init__(self)
+        
+        self.host = b"localhost"
+        self.port = 4712
+        self.passwd = None
+
         self.prompt = "> "
         self.intro = "Welcome to %s %s" % (prog_name, prog_ver)
         self.ec = None
+        
+        if len(sys.argv) > 1:
+            try:
+                options, args = getopt(sys.argv[1:], 'vhs:p:w:', ["version", "help", "server=","port=","password="])
+
+                for option, argument in options:
+                    if option in ("-v", "--version"):
+                        print (prog_name, prog_ver)
+                        exit()
+                    elif option in ("-h", "--help"):
+                        self.print_help()
+                        exit()
+                    elif option in ("-s", "--server"):
+                        self.host = argument.encode(encoding='utf_8', errors='strict')
+                    elif option in ("-p", "--port"):
+                        self.port = argument
+                    elif option in ("-w", "--password"):
+                        self.passwd = argument.encode(encoding='utf_8', errors='strict')
+
+            except GetoptError:
+                print ('Unknown Argument(s) "%s"'," ".join(sys.argv[1:]))
+                self.print_help()
+                exit()
+                
+        cmd.Cmd.__init__(self)
+        
+    def print_help(self):
+            print ()
+            print (prog_name,"v"+prog_ver)
+            print ()
+            if (sys.argv[0].endswith(".py")):
+                print ("Usage: python",prog_name+".py","[options]")
+            else:
+                print ("Usage:",prog_name,"[options]")
+            print ()
+            print ("<Options>")
+            print ("  -v, --version", " " * 20, "Print version to terminal")
+            print ("  -s, --server=<server name or ip>", " " * 1, "Set domain name or ip of the aMule Server")
+            print ("  -p, --port=<port number>", " " * 9, "Set port number of the server")
+            print ("  -h, --help", " " * 23, "Display this help screen")
+            print ()
+        
     def preloop(self):
-        passwd = getpass.getpass("Password: ").encode(encoding='utf_8', errors='strict')
+        if self.passwd == None:
+            self.passwd = getpass.getpass("Password: ").encode(encoding='utf_8', errors='strict')
+        
         try:
-            self.ec = ec.conn(passwd, host, port, prog_name, prog_ver)
+            self.ec = ec.conn(self.passwd, self.host, self.port, prog_name, prog_ver)
         except ec.ConnectionFailedError:
             print("Connection failed")
             sys.exit()
